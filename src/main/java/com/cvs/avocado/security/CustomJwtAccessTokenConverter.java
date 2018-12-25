870f2aff9c562025527b65b1f9008cc1059574ff
adityanikesh <adityanikesh@gmail.com>
2018-12-26 00:20:45 +0530

    Overridden constructor to set key pair.

--- src/main/java/com/cvs/avocado/security/CustomJwtAccessTokenConverter.java
+++ src/main/java/com/cvs/avocado/security/CustomJwtAccessTokenConverter.java
@@ -1,26 +1,14 @@
 package com.cvs.avocado.security;
 
-import java.security.KeyPair;
-
 import org.springframework.core.io.ClassPathResource;
-import org.springframework.security.oauth2.common.OAuth2AccessToken;
-import org.springframework.security.oauth2.provider.OAuth2Authentication;
 import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
 import org.springframework.security.oauth2.provider.token.store.KeyStoreKeyFactory;
 
-public class CustomJwtAccessTokenConverter extends JwtAccessTokenConverter{
-
-//	@Override
-//	public OAuth2AccessToken enhance(OAuth2AccessToken accessToken, OAuth2Authentication authentication) {
-//		accessToken.
-//		return null;
-//	}
+public class CustomJwtAccessTokenConverter extends JwtAccessTokenConverter {
 	
-	@Override
-	public void setKeyPair(KeyPair keyPair) {
+	public CustomJwtAccessTokenConverter(){
 		KeyStoreKeyFactory keyStoreKeyFactory = new KeyStoreKeyFactory(
                 new ClassPathResource("tomcat-keystore.p12"), "changeit".toCharArray());
 		this.setKeyPair(keyStoreKeyFactory.getKeyPair("tomcat"));
 	}
-
 }
